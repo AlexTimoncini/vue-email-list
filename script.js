c7db7f47662ec6236bb6d3ccc616f7516a86fc80
Alex Timoncini <126895091+AlexTimoncini@users.noreply.github.com>
2023-06-13 14:36:38 +0200

    added email counter to make the app more flexible

--- script.js
+++ script.js
@@ -4,11 +4,12 @@ createApp({
     data() {
         return {
             emailList: [],
+            emailVisible: 10,
         }
     },
     methods: {
         getRandomEmailFromApi(){
-            for(let i = 0; i < 10; i++){
+            for(let i = 0; i < this.emailVisible; i++){
                 axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((response)=>{                
                     this.emailList.push(response.data.response);
                 });
