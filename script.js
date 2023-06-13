2bb7384e748fc8b18f1114ba88b079459d91fcf9
Alex Timoncini <126895091+AlexTimoncini@users.noreply.github.com>
2023-06-13 14:26:29 +0200

    displayed 10 emails with api

---
+++ script.js
@@ -0,0 +1,21 @@
+const { createApp } = Vue
+
+createApp({
+    data() {
+        return {
+            emailList: [],
+        }
+    },
+    methods: {
+        getRandomEmailFromApi(){
+            for(let i = 0; i < 10; i++){
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((response)=>{                
+                    this.emailList.push(response.data.response);
+                });
+            }
+        }
+    },
+    created(){
+        this.getRandomEmailFromApi()
+    }
+}).mount('#app')
